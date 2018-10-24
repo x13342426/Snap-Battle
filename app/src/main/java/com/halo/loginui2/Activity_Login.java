b1d8d02d67ce758b614e423a2d02c527ac6ec2ba
Bren <x13342426@student.ncirl.ie>
2018-10-24 10:14:52 +0100

    Home screen animation + GUI linked to register page, pre SQLITE db

---
+++ app/src/main/java/com/halo/loginui2/Activity_Login.java
@@ -0,0 +1,42 @@
+package com.halo.loginui2;
+
+import android.content.Intent;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.RelativeLayout;
+
+public class Activity_Login extends AppCompatActivity {
+
+    RelativeLayout rellay1, rellay2;
+
+    Handler handler = new Handler();
+    Runnable runnable = new Runnable() {
+        @Override
+        public void run() {
+            rellay1.setVisibility(View.VISIBLE);
+            rellay2.setVisibility(View.VISIBLE);
+        }
+    };
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        rellay1 = (RelativeLayout) findViewById(R.id.rellay1);
+        rellay2 = (RelativeLayout) findViewById(R.id.rellay2);
+
+        handler.postDelayed(runnable, 2000); //2000 is the timeout for the splash
+    }
+
+    public void onButtonClick(View v){
+        if (v.getId() == R.id.BdDisplay)
+        {
+            Intent i = new Intent(Activity_Login.this, register.class);
+            startActivity(i);
+        }
+
+    }
+}
---
+++ app/src/main/java/com/halo/loginui2/register.java
@@ -0,0 +1,13 @@
+package com.halo.loginui2;
+
+import android.os.Bundle;
+
+public class register extends Activity_Login {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.register);
+    }
+}
